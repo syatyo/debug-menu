956c85aed027fa0f07e456173bf3163db8895154
syatyo <yoshiharuseijin@gmail.com>
2019-05-18 01:46:34 +0900

    feat: 🎸 Create framework project

---
+++ debug-menu/debug-menu/debug_menu.h
@@ -0,0 +1,19 @@
+//
+//  debug_menu.h
+//  debug-menu
+//
+//  Created by syatyo on 2019/05/18.
+//  Copyright © 2019 syatyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for debug_menu.
+FOUNDATION_EXPORT double debug_menuVersionNumber;
+
+//! Project version string for debug_menu.
+FOUNDATION_EXPORT const unsigned char debug_menuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <debug_menu/PublicHeader.h>
+
+
